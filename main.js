143752432d633d975a5df91a8a34b726c69781ab
Praneet0212 <71916363+Praneet0212@users.noreply.github.com>
2021-01-01 19:02:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -29,11 +29,11 @@ var last_position_of_x, last_position_of_y;
         ctx.lineWidth = width_of_line;
 
         console.log("Last position of x and y coordinates = ");
-        console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
+        console.log("x = " + last_position_of_x + " y = " + last_position_of_y);
         ctx.moveTo(last_position_of_x, last_position_of_y);
 
         console.log("Current position of x and y coordinates = ");
-        console.log("x  = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
+        console.log("x  = " + current_position_of_mouse_x + " y = " + current_position_of_mouse_y);
         ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
         ctx.stroke();
         }
